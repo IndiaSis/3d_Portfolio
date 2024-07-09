982ebb0eb7139a4971de76acb3a15bac70c614a2
Siser Pratap <siserinsevoc@gmail.com>
2024-07-10 05:06:38 +0530

    initialzed hero sectioni

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Suspense, useState, useEffect } from "react";
+
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 const Computers = () => {
+
+  const Computer=useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
+   <mesh>
+    <hemisphereLight intensity={0.15} groundColor="black" />
+    <pintLight intensity={1} />
+    <primitive object={Computer.scene} />
+
+   </mesh> 
   )
 }
 
