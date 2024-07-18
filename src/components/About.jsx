fa6f65a4157691064febc3d3ca5004a12fda9fd0
Siser Pratap <siserinsevoc@gmail.com>
2024-07-19 01:42:40 +0530

    Added the Animation anf gestures in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Tilt } from 'react-tilt';
-import { motion, px } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { services } from '../constants';
+import {SectionWrapper} from '../hoc';
 import { fadeIn, textVariant } from '../utils/motion';
 
 
@@ -44,7 +45,7 @@ const About=()=>{
   )
 }
 
-export default About;
+export default SectionWrapper(About, "about");
 
 
 
