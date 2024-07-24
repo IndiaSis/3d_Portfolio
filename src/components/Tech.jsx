1706921e15a5ebd3b993e18c7d6b25dab6d37891
Siser Pratap <siserinsevoc@gmail.com>
2024-07-24 22:52:24 +0530

    Added the fadein Variation to Styles icon

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
 
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 
+
+const Icon = ({icon, name, index}) => (
+    
+        <div className=" bg-tertiary rounded-[20px] py-5 px-12 flex items-center justify-evenly flex-col">
+        <motion.div variants={fadeIn("right", "spring", 0.1*index, 3)}>
+        <img className="p-2 w-28 h-28 " src={icon} alt={name} />
+        <h2 className=" w-auto h-auto items-center ">{name}</h2>
+        </motion.div>
+        </div>
+    
+);
+
+
 const Tech = () =>{
     return (
         <>
-             <div className="flex flex-row flex-wrap justify-center gap-10">
-                {technologies.map((technologies)=>(
-                <div className="w-28 gap-3 mb-3 h-28 bg-tertiary bg-opacity-25 flex flex-col items-center border-none rounded-[20px]">
-                    <img className="p-2" src={technologies.icon} alt={technologies.name} />
-                    <h2 className=" items-center ">{technologies.name}</h2>
-                </div>
+             <div className="flex mt-20 flex-row flex-wrap justify-evenly items-center gap-10">
+                {technologies.map((technology, index)=>(
+                <Icon key={technology.name} index={index} {...technology}/>
         ))}
         </div>  
         </>
