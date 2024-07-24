f6a95cccde1f574c192c37c4d57fda06dfbbc3f6
Siser Pratap <siserinsevoc@gmail.com>
2024-07-24 23:05:51 +0530

    Added the skills functionalities in project Card

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -10,9 +10,9 @@ import { technologies } from "../constants";
 const Icon = ({icon, name, index}) => (
     
         <div className=" bg-tertiary rounded-[20px] py-5 px-12 flex items-center justify-evenly flex-col">
-        <motion.div variants={fadeIn("right", "spring", 0.1*index, 3)}>
+        <motion.div className="flex justify-center items-center flex-col" variants={fadeIn("right", "spring", 0.1*index, 1)}>
         <img className="p-2 w-28 h-28 " src={icon} alt={name} />
-        <h2 className=" w-auto h-auto items-center ">{name}</h2>
+        <h2 className=" w-auto h-auto  ">{name}</h2>
         </motion.div>
         </div>
     
